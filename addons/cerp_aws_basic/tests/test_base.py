1ea72426c2ad84780d17a65914842dfa63c25bf1
Ryan Northey <ryan@synca.io>
2020-07-13 17:27:47 +0100

    Add cerp_aws_basic

---
+++ addons/cerp_aws_basic/tests/test_base.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo.tests.common import SavepointCase
+
+
+class TestCloudERPBase(SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestCloudERPBase, cls).setUpClass()
